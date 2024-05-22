f83e454c5220784b6835a4b4378781ac4d8e275f
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-05-22 17:46:07 -0300

    feat: change column datas

--- apps/web/src/components/Container.tsx
+++ apps/web/src/components/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export function Container(props: { children: ReactNode; className?: string }) {
 	return (
-		<div className={`w-full max-w-[800px] ${props.className ?? ''}`}>
+		<div className={`w-full max-w-[1280px] ${props.className ?? ''}`}>
 			{props.children}
 		</div>
 	)
--- apps/web/src/layout/DataTable/columns.tsx
+++ apps/web/src/layout/DataTable/columns.tsx
@@ -1,5 +1,6 @@
 import { Checkbox } from '@/components/ui/checkbox'
 import { ColumnDef } from '@tanstack/react-table'
+import moment from 'moment'
 import { Invoice } from './data'
 
 export const columns: ColumnDef<Invoice>[] = [
@@ -25,19 +26,42 @@ export const columns: ColumnDef<Invoice>[] = [
 		),
 	},
 	{
+		accessorKey: 'month',
+		header: 'Mês referente',
+		cell: ({ row }) => {
+			const month = moment(row.getValue('month')).local().format('MMM/YYYY')
+
+			return month
+		},
+	},
+	{
 		accessorKey: 'n_client',
 		header: 'Nº Cliente',
 	},
 	{
-		accessorKey: 'electricity_qtd',
-		header: 'Consumo (kWh)',
+		header: 'Consumo de Energia Elétrica (kWh)',
+		cell: ({ row }) => {
+			const electricity_qtd = row.original.electricity_qtd || 0
+			const exemptEnergy_qtd = row.original.exemptEnergy_qtd || 0
+
+			return electricity_qtd + exemptEnergy_qtd
+		},
 	},
 	{
 		accessorKey: 'compensatedEnergy_qtd',
-		header: 'Compensado (kWh)',
+		header: 'Energia Compensada (kWh)',
 	},
 	{
-		accessorKey: 'total',
-		header: 'Total (R$)',
+		header: 'Valor total sem GD (R$)',
+		cell: ({ row }) => {
+			const electricity_total = row.original.electricity_total || 0
+			const exemptEnergy_total = row.original.exemptEnergy_total || 0
+			const contribution = row.original.contribution
+
+			return new Intl.NumberFormat('pt-BR', {
+				style: 'currency',
+				currency: 'BRL',
+			}).format(electricity_total + exemptEnergy_total + contribution)
+		},
 	},
 ]
