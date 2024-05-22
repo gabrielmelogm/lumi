facd9cf4a84acaaaa1a58d883730b03ceb379dec
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-05-22 12:36:46 -0300

    feat: init web project

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'green': '#023b21',
+      'gray': '#d9d9d9',
+      'dark-gray': '#4d4d4d',
+      'white': '#ffffff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
