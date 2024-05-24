525a3b7bd095f45c243b1ea50ea6bf995907783e
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-05-24 15:21:24 +0000

    ci: vpc to deploy aws

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,19 @@ FROM node:20-slim
 
 RUN \
   apt-get update -y && \
-  apt-get install -y openssl
+  apt-get install -y openssl unzip wget
+
+# Install Terraform
+ARG TERRAFORM_VERSION=1.8.4
+RUN wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+RUN unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin/
+RUN terraform --version
+
+# Install aws cli
+RUN wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+RUN unzip awscli-exe-linux-x86_64.zip
+RUN ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 
 WORKDIR /home/node/app
 
---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name = "key"
+  public_key = file("./key.pem.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "lumi" {
+  ami = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = "Lumi"
+  }
+}
